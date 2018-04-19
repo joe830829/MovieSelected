60fb6f28cdfc87d67f1ab8660b9ffaadce4ba3f7
new <joe_0829@hotmail.com>
2018-04-19 16:45:34 +0800

    output

--- app/src/main/java/com/example/anew/movieselected/inoutActivity.java
+++ app/src/main/java/com/example/anew/movieselected/inoutActivity.java
@@ -2,12 +2,27 @@ package com.example.anew.movieselected;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class inoutActivity extends AppCompatActivity {
-
+    TextView output;
+    Button btn_output;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inout);
+
+        output = (TextView)findViewById(R.id.output);
+        btn_output = (Button)findViewById(R.id.btn_output);
+
+        btn_output.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                
+            }
+        });
+
     }
 }
